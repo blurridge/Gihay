959dcf819f653ce2ce9924fc2d38771963aaba69
Zach Riane Machacon <zachriane01@gmail.com>
2024-05-01 13:38:14 +0800

    feat: added navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,15 +11,22 @@ export default {
         "gihay-highlight": "#F63E7B",
         "gihay-dark-pink": "#940D3E",
         "gihay-heading": "#F63E7B",
+        customBorder: "rgba(255, 255, 255, 0.125)",
       },
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
         glacial: ["GlacialIndifference", "sans-serif"],
-		bright: ["Bright", "sans-serif"],
+        bright: ["Bright", "sans-serif"],
       },
       fontWeight: {
         normal: 400,
         bold: 700,
+      },
+      borderRadius: {
+        xl: "12px",
+      },
+      borderWidth: {
+        1: "1px",
       },
     },
   },
